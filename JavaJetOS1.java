8aa9eddf8b3f15740e7740d84e98da9df729a1ea
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-09 15:49:15 +0530

    removed unusefull resources

---
+++ JavaJetOS1.java
@@ -0,0 +1,164 @@
+import java.util.Scanner;
+
+
+
+
+
+public class JavaJetOS1
+{
+    public static void main(String[] args)
+    {
+        int AppToRun;
+        double a;
+        double b;
+        double x;
+        double y;
+        int RunCalcAgain;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Welcome to JavaJetOS! Please enter your username: ");
+            String username = sc.nextLine();
+            System.out.println("Please enter your password: ");
+            String password = sc.nextLine();
+            if (username.equals("default") && password.equals("Pass@123") )
+            {
+                System.out.println("Logged in and validated successfully! ");
+                int RunAppAgain = 1;
+                while (RunAppAgain == 1){
+                    System.out.println("Please enter the number of the application you would like to run: \n1)Calculator");
+                    AppToRun= sc.nextInt();
+                    if (AppToRun==1)
+                    {
+                    
+
+                        System.out.println("Which operation do you want to use: \n1.Multiplacation\n2.Divison\n3.Addition\n4.Substraction\n5.Square\n6.Sqaure Root\nEnter the Option Number: ");
+                        double op1 = (double)sc.nextInt();
+            
+                        if (op1 == 1) {
+                            System.out.println("Enter your first value(a): ");
+                            a = sc.nextDouble();
+                            System.out.println("Enter your second value(b): ");
+                            b = sc.nextDouble();
+                            x = a * b;
+                            System.out.println("Your product is: " + x);
+                        }
+
+                        if (op1 == 2.0) 
+                        {
+                            System.out.println("Enter your first value(a): ");
+                            a = sc.nextDouble();
+                            System.out.println("Enter your second value(b): ");
+                            b = sc.nextDouble();
+                            x = a / b;
+                            y = a % b;
+                            System.out.println("The quotient is: " + x + "while the remainder is: " + y);
+                        }
+
+                        if (op1 == 3.0) {
+                            System.out.println("Enter your first value(a): ");
+                            a = sc.nextDouble();
+                            System.out.println("Enter your second value(b): ");
+                            b = sc.nextDouble();
+                            x = a + b;
+                            System.out.println("The sum is: " + x);
+                        }
+
+                        if (op1 == 4.0) {
+                            System.out.println("Enter your first value(a): ");
+                            a = sc.nextDouble();
+                            System.out.println("Enter your second value(b): ");
+                            b = sc.nextDouble();
+                            x = a - b;
+                            System.out.println("The answer is: " + x);
+                        }
+
+                        if (op1 == 5.0) {
+                            System.out.println("Enter your value(a): ");
+                            a = sc.nextDouble();
+                            x = a * a;
+                            System.out.println("The square is: " + x);
+                        }
+
+                        if (op1 == 6.0) {
+                            System.out.println("Enter your value(a): ");
+                            a = sc.nextDouble();
+                            x = Math.sqrt(a);
+                            System.out.println("The answer is:  " + x);
+                        }
+                        System.out.println("Would you like to repeat the program?1.Yes or 2.No\nEnter Option No: ");
+                        RunCalcAgain = sc.nextInt();
+
+                        while(RunCalcAgain==1)
+                        {
+                            System.out.println("Which operation would you want to use: \n1.Multiplacation\n2.Divison\n3.Addition\n4.Substraction\n5.Square\n6.Sqaure Root\nEnter the Option Number: ");
+                            op1=sc.nextInt();
+                            if (op1==1)
+                            {
+                                System.out.println("Enter your first value(a): ");
+                                a = sc.nextDouble();
+                                System.out.println("Enter your second value(b): ");
+                                b = sc.nextDouble();
+                                x = a * b;
+                                System.out.println("Your product is: " + x);
+                            }
+
+                            if (op1 == 2.0) {
+                                System.out.println("Enter your first value(a): ");
+                                a = sc.nextDouble();
+                                System.out.println("Enter your second valueb): ");
+                                b = sc.nextDouble();
+                                x = a / b;
+                                y = a % b;
+                                System.out.println("The quotient is: " + x + "while the remainder is: " + y);
+                            }
+
+                            if (op1 == 3.0) {
+                                System.out.println("Enter your first value(a): ");
+                                a = sc.nextDouble();
+                                System.out.println("Enter your second value(b): ");
+                                b = sc.nextDouble();
+                                x = a + b;
+                                System.out.println("The sum is: " + x);
+                            }
+
+                            if (op1 == 4.0) {
+                                System.out.println("Enter your first value(a): ");
+                                a = sc.nextDouble();
+                                System.out.println("Enter your second value(b): ");
+                                b = sc.nextDouble();
+                                x = a - b;
+                                System.out.println("The answer is: " + x);
+                            }
+
+                            if (op1 == 5.0) {
+                                System.out.println("Enter your value(a): ");
+                                a = sc.nextDouble();
+                                x = a * a;
+                                System.out.println("The square is: " + x);
+                            }
+
+                            if (op1 == 6.0) {
+                                System.out.println("Enter your value(a): ");
+                                a = sc.nextDouble();
+                                x = Math.sqrt(a);
+                                System.out.println("The answer is:  " + x);
+                            }
+                            System.out.println("Would you like to repeat the program?1.Yes or 2.No\nEnter Option No: ");
+                            RunCalcAgain = sc.nextInt();
+                        }
+
+                        if(RunCalcAgain==2){
+                            System.out.println("Would you like to run another app or shut down the OS?\nPress 1 to run another app and press 0 to shut down the OS.");
+                            RunAppAgain = sc.nextInt();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+       
+
+
+
