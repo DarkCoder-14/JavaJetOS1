5be5a22e6e1b0ed556f396d11585ed15e67494f4
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-09 23:30:47 +0530

    not working, have to make changes
    No idea, why its not working, have to make changes tom, on call.

--- backupfiles.java
+++ backupfiles.java
@@ -1,4 +1,3 @@
-import java.awt.Desktop;
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -6,13 +5,13 @@ import java.io.FilenameFilter;
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.util.Scanner;
 
-public class files 
+public class backupfiles 
 {
     public static void main(String[] args) 
     {
             Scanner sc=new Scanner(System.in);
             int optionfile;
-            System.out.println("Do you want to 1. Create a File or 2. Open a File\nEnter Option: ");
+            System.out.println("Do you want to 1. Create a File or 2. Open a File\n Enter Option: ");
             optionfile=sc.nextInt();
             if(optionfile==1)
             {
@@ -78,28 +77,8 @@ public class files
                             System.out.println("finally1");
                         }
                     }
-                    String openfilen;
-                    if(optionfile==2)
+                    else
                     {
-                        System.out.println("Enter filename of file you want to open: ");
-                        openfilen=sc.nextLine();
-                        try  
-                        { 
-                        
-                        File file = new File(openfilen);   
-                        if(!Desktop.isDesktopSupported())//check if Desktop is supported by Platform or not  
-                        {  
-                        System.out.println("not supported");  
-                        return;  
-                        }  
-                        Desktop desktop = Desktop.getDesktop();  
-                        if(file.exists())         //checks file exists or not  
-                        desktop.open(file);              //opens the specified file  
-                        }  
-                        catch(Exception e)  
-                        {  
-                        e.printStackTrace();  
-                        }
 
                     }
                                         
