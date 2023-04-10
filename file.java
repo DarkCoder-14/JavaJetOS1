ee2391536bbd268402081ec25017c19ec9c9ae32
DarkCoder-14 <ojaswin.jha@gmail.com>
2023-04-10 21:45:23 +0530

    new repository for organizatiom

---
+++ file.java
@@ -0,0 +1,16 @@
+
+public class file {
+
+    public static Object getParentFile() {
+        return null;
+    }
+
+    public static boolean isFile() {
+        return false;
+    }
+
+    public static boolean createNewFile() {
+        return false;
+    }
+
+}
---
+++ filestest.java
@@ -0,0 +1,122 @@
+import java.awt.Desktop;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;  
+import java.util.Scanner;
+
+public class filestest
+{
+    public static void main(String[] args) 
+    {
+            Scanner sc=new Scanner(System.in);
+            if (file.getParentFile() != null && !((Object) file.getParentFile()).mkDirs()) {
+                // handle permission problems here
+            }
+            // either no parent directories there or we have created missing directories
+            if (file.createNewFile() || file.isFile()) {
+                // ready to write your content
+            } else {
+                // handle directory here
+            }
+            int optionfile;
+            System.out.println("Do you want to 1. Create a File or 2. Open a File\nEnter Option: ");
+            optionfile=sc.nextInt();
+            if(optionfile==1)
+            {
+                try  
+                {
+                    System.out.println("Enter your filename: ");
+                    String filename;
+                    filename=sc.nextLine();  
+                    File myObj = new File(filename);
+                    try
+                    {
+                    
+                    if (myObj.createNewFile()) 
+                    {
+                        System.out.println("File created: " + myObj.getName());
+                    } 
+                    else 
+                    {
+                        System.out.println("File already exists.");
+                    }
+                    } 
+                    catch (IOException e)
+                    {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                    }
+                    try 
+                    {
+                        FileWriter myWriter = new FileWriter(myObj);
+                        System.out.println("Enter the text you want to save: ");
+                        String text= sc.nextLine();
+                        myWriter.write(text);
+                        myWriter.close();
+                        System.out.println("Successfully wrote to the file.");
+                    } 
+                    catch (IOException e) 
+                    {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
+                    System.out.println("Do you want to read the text you saved in your file: \n1.Yes\n2.No\nEnter Option No: ");
+                    int readop;
+                    readop=sc.nextInt();
+                    if (readop==1)
+                    {
+                        try 
+                        {
+                            Scanner myReader = new Scanner(myObj);
+                            while (myReader.hasNextLine()) 
+                            {
+                            String data = myReader.nextLine();
+                            System.out.println("\n\n"+data);
+                            }
+                            myReader.close();
+                        }   
+                        catch (FileNotFoundException e) 
+                        {
+                            System.out.println("An error occurred.");
+                            e.printStackTrace();
+                        }
+                        finally
+                        {
+                            System.out.println("finally1");
+                        }
+                    }
+                    String openfilen;
+                    if(optionfile==2)
+                    {
+                        System.out.println("Enter filename of file you want to open: ");
+                        openfilen=sc.nextLine();
+                        try  
+                        { 
+                        
+                        File file = new File(openfilen);   
+                        if(!Desktop.isDesktopSupported())//check if Desktop is supported by Platform or not  
+                        {  
+                        System.out.println("not supported");  
+                        return;  
+                        }  
+                        Desktop desktop = Desktop.getDesktop();  
+                        if(file.exists())         //checks file exists or not  
+                        desktop.open(file);              //opens the specified file  
+                        }  
+                        catch(Exception e)  
+                        {  
+                        e.printStackTrace();  
+                        }
+
+                    }
+                                        
+                }
+                catch(Exception ex)
+                {}
+                finally
+                {}
+            }
+        }
+    }
